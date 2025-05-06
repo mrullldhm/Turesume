006cfb2353fa1e024137b20ea9293014fb1c2801
mrullldhm <mrullldhm94@gmail.com>
2025-05-07 03:29:23 +0800

    Navbar design part-1

--- client/src/app/(main)/resumes/navbar.tsx
+++ client/src/app/(main)/resumes/navbar.tsx
@@ -1,5 +1,21 @@
+import Image from "next/image";
+import Link from "next/link";
+import logo from "@/assets/logo.png"
+
 export default function Navbar() {
     return (
-        <header>Navbar Here</header>
+        <header className="shadow-sm">
+            <div className="max-w-7xl mx-auto p-3 flex items-center justify-between gap-3">
+                <Link href="/resumes" className="flex items-center gap-2">
+                   <Image 
+                    src={logo}
+                    alt="logo" 
+                    width={35} 
+                    height={35} 
+                    className="rounded-full"
+                    />
+                </Link>
+            </div>
+        </header>
     );
 }
