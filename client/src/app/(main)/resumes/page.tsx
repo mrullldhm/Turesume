882943a97e2ad0eddd2cf39b6853ff5cf8da9edf
mrullldhm <mrullldhm94@gmail.com>
2025-05-11 03:38:43 +0800

    create resume page

--- client/src/app/(main)/resumes/page.tsx
+++ client/src/app/(main)/resumes/page.tsx
@@ -1,13 +1,22 @@
 import { Metadata } from "next";
-
+import { Button } from "@/components/ui/button";
+import { PlusSquare } from "lucide-react";
+import Link from "next/link";
 
 export const metadata: Metadata = {
-    title: "Editor",
+    title: "Resumes",
     description: "Resumes editor page",
 }
 
 export default function Page() {
     return (
-        <div>Hello, Resume page!</div>
+        <main className="mx-auto w-full max-w-7xl space-y-6 px-3 py-6">
+            <Button asChild className="mx-auto flex w-fit gap-2">
+                <Link href="/editor">
+                    <PlusSquare className="size-5" />
+                    Create resume
+                </Link>
+            </Button>
+        </main>
     )
 }
