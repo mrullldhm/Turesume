7be75761987554125e9d6e22c04f4e9bcf1f9c6e
mrullldhm <mrullldhm94@gmail.com>
2025-05-11 19:40:23 +0800

    change the root font

---
+++ client/src/app/(main)/editor/page.tsx
@@ -0,0 +1,9 @@
+import { Metadata } from "next"
+
+export const metadata : Metadata = {
+    title: "Editor",
+    description: "Editor page"
+}
+
+export default function Page() {
+}
--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,10 +1,13 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Poppins } from 'next/font/google';
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { ThemeProvider } from "next-themes";
 
-const inter = Inter({ subsets: ["latin"] });
+const poppins = Poppins({ 
+  weight: ['400', '700'], // Specify weights
+  subsets: ['latin'], 
+});
 
 export const metadata: Metadata = {
   title: {
@@ -23,7 +26,7 @@ export default function RootLayout({
     // 
     <ClerkProvider>
     <html lang="en" suppressHydrationWarning>
-      <body className={inter.className}>
+      <body className={poppins.className}>
         <ThemeProvider 
         attribute="class" 
         defaultTheme="system" 
