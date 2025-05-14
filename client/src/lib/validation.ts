3019837fd711c0380919eee45a66164cdd2c9437
mrullldhm <mrullldhm94@gmail.com>
2025-05-15 03:03:28 +0800

    Add Personal Info schema

--- client/src/lib/validation.ts
+++ client/src/lib/validation.ts
@@ -3,11 +3,33 @@ import { z } from "zod";
 // A string that can be empty, undefined, or a trimmed string
 export const optionalString = z.string().trim().optional().or(z.literal(""));
 
-// Schema for general information with optional title and description
+// General Information Schema
 export const generalInfoSchema = z.object({
   title: optionalString,
   description: optionalString,
 });
-
 // TypeScript type automatically generated from the schema
 export type GeneralInfoValues = z.infer<typeof generalInfoSchema>;
+
+
+// Personal Information Schema
+export const personalInfoSchema = z.object({
+  firstName: optionalString,
+  lastName: optionalString,
+  jobTittle: optionalString,
+  city: optionalString,
+  country: optionalString,
+  phone: optionalString,
+  email: optionalString,
+  photo: z.custom<File | undefined>()
+  .refine(
+    (file) => !file || (file instanceof File && file.type.startsWith("image/")),
+    "Must be a valid image file"
+  )
+  .refine(
+    (file) => !file || (file && file.size <= 1024 * 1024 * 4),
+    "File size must be less than 4MB"
+  )
+});
+// TypeScript type automatically generated from the schema
+export type PersonalInfoValues = z.infer<typeof personalInfoSchema>;
