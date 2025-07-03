f238af2f17adb60b29aa82a699c3721c81f0a14f
mrullldhm <mrullldhm94@gmail.com>
2025-07-03 19:57:45 +0800

    Update the layout.tsx, eslint.config.mjs and prettier.config.js

--- client/eslint.config.mjs
+++ client/eslint.config.mjs
@@ -10,7 +10,17 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends("next/core-web-vitals", "next/typescript", "prettier"),
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          ignoreRestSiblings: true,
+        },
+      ],
+    },
+  }
 ];
 
 export default eslintConfig;
