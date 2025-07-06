e63c460b3fc4218703e2b054759e478ef2fb6a02
mrullldhm <mrullldhm94@gmail.com>
2025-07-06 18:34:28 +0800

    Update button Create Resume

--- client/src/app/(main)/resumes/page.tsx
+++ client/src/app/(main)/resumes/page.tsx
@@ -1,6 +1,5 @@
 import { Metadata } from "next";
 import { Button } from "@/components/ui/button";
-import { PlusSquare } from "lucide-react";
 import Link from "next/link";
 import { auth } from "@clerk/nextjs/server";
 import prisma from "@/lib/prisma";
@@ -42,7 +41,6 @@ export default async function Page() {
     <main className="mx-auto w-full max-w-7xl space-y-6 px-3 py-6">
       <Button asChild className="mx-auto flex w-fit gap-2">
         <Link href="/editor">
-          <PlusSquare className="size-5" />
           Create Resume
         </Link>
       </Button>
